3227694aa5deeaea874fdee52284220dff893926
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-09 15:59:38 -0400

    styling it

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
-  # read all
-  get 'tasks', to: 'tasks#index'
-  # create
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
-  # show one
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # update
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  # destroy
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
