077d18fdec5bb1081ed3ceee6705368db026b017
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-09 12:29:22 -0400

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # read all
+  get 'tasks', to: 'tasks#index'
+  # show one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'task#create'
 end
