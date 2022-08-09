548ee87eaa1f86844fdf2227e066c44cd9cfe413
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-09 13:58:23 -0400

    added update/edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
   # create
   get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'task#create'
+  post 'tasks', to: 'tasks#create'
   # show one
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 end
