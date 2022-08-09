fc6847dbea00f7e2b6e0974d5f59ee7dc8f781b2
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-09 12:37:29 -0400

    added create in controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # read all
   get 'tasks', to: 'tasks#index'
-  # show one
-  get 'tasks/:id', to: 'tasks#show', as: :task
   # create
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'task#create'
+  # show one
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
